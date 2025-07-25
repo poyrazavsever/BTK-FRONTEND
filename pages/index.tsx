b87f7d8f4e71e60d31da348e0a00b865b00d8c27
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-25 18:57:46 +0300

    feat: created file system

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App</h1>
+      <p>This is the home page.</p>
+      <p>Explore the app and enjoy!</p>
+    </div>
+  );
+}
