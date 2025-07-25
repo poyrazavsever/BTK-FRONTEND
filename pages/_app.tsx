e4ce12897efb350fed08025fc078f519c5a70625
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-26 02:25:55 +0300

    feat: updated layout system with props like isAuthPage
    for remove layout or added another layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,15 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout/layout";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & { Component: any }) {
+  // Eğer sayfa componenti isAuthPage prop'una sahipse, Layout'u sarmadan döndür
+  const isAuthPage = Component.isAuthPage;
+  if (isAuthPage) {
+    return <Component {...pageProps} />;
+  }
   return (
     <Layout>
       <Component {...pageProps} />
