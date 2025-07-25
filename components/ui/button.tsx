d48a2d2e533ead22a07b33a93cbea1b9d469f31d
poyrazavsever <poyrazavsever@hotmail.com>
2025-07-25 19:47:51 +0300

    fix(style): fixed width problem in btn

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -16,12 +16,12 @@ const Button: React.FC<ButtonProps> = ({
   className = "",
 }) => {
   let baseStyle =
-    "w-full py-3 px-6 rounded-md font-poppins font-regu text-lg transition-all duration-200 flex items-center justify-center";
+    "py-2 px-6 rounded-md font-poppins text-base cursor-pointer transition-all duration-200 flex items-center justify-center";
   let variantStyle = "";
 
   switch (variant) {
     case "primary":
-      variantStyle = "bg-primary text-primary-foreground hover:bg-primary/90";
+      variantStyle = "bg-primary text-background hover:bg-primary/90";
       break;
     case "secondary":
       variantStyle = "bg-gray-200 text-gray-600 hover:bg-gray-300";
